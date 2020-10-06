aed5f7e45f6a58f6bf87af8ab829b5b40ac625bc
Nikitka <user123@gmail.com>
2020-10-06 18:23:31 +0300

    s

---
+++ Class Uravnenie/Class Uravnenie/Class Uravnenie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace class_uravnenie
+{
+    class Uravnenie
+    {
+        public double a;
+        public double b;
+
+        public double Reshenie(double a, double b)
+        {
+            return b / a;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uravnenie mU = new Uravnenie();
+            Console.WriteLine("Введем данные уравния типа a*x=b");
+            Console.WriteLine("a = ");
+            mU.a = Convert.ToDouble(Console.ReadLine());
+            if (mU.a == 0) 
+                { 
+                    Console.WriteLine("Уравнение не имеет корней"); 
+                    return;
+                }
+            Console.WriteLine("b = ");
+            mU.b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("X = " + mU.Reshenie(mU.a, mU.b));
+        }
+    }
+}
